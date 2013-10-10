9e3fb46c5436279d3e604beb2fd8007071cf98c1
Örjan Sjöholm <orjansjoholm@gmail.com>
2013-10-10 13:25:12 +0200

    Initial commit

---
+++ NLog0MQ.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NLog0MQ.Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoMagic();
+
+            GC.Collect();
+
+            Console.ReadLine();
+        }
+
+        public static void DoMagic()
+        {
+            var log = NLog.LogManager.GetLogger("NLog0MQ.Example");
+            log.Info("Hello...");
+           
+        }
+    }
+}
---
+++ NLog0MQ/NLog0MqTarget.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NLog;
+using NLog.Targets;
+using ZeroMQ;
+
+namespace NLog0MQ
+{
+    /// <summary>
+    ///     https://github.com/nlog/NLog/wiki/How%20to%20write%20a%20Target
+    /// </summary>
+    [Target("NLog0Mq")]
+    public class NLog0MqTarget : TargetWithLayout
+    {
+        private ZmqContext context;
+        private ZmqSocket publisher;
+        private bool targetClosed;
+        private readonly object locker;
+
+        public string Host { get; set; }
+
+        public NLog0MqTarget()
+        {
+            locker = new object();
+        }
+
+        ~NLog0MqTarget()
+        {
+            CloseTarget();
+        }
+        
+        protected override void InitializeTarget()
+        {
+            context = ZmqContext.Create();
+            publisher = context.CreateSocket(SocketType.PUB);
+            publisher.Bind("tcp://*:5563");
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            throw new Exception("sadfdsafdsafds");
+        }
+
+        protected override void CloseTarget()
+        {
+            lock (locker)
+            {
+                if (targetClosed)
+                    return;
+                
+                if (publisher != null)
+                {
+                    publisher.Close();
+                    publisher.Dispose();
+                    publisher = null;
+                }                
+                
+                if (context != null)
+                {
+                    context.Dispose();
+                    context = null;
+                }
+
+                targetClosed = true;
+            }
+        }
+    }
+}
