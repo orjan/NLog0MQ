94ef4e535748af129ed1cd3cbcb2813402a6f022
Örjan Sjöholm <orjansjoholm@gmail.com>
2013-10-14 22:28:08 +0200

    Sending example messages from a background thread

--- NLog0MQ.Example/Program.cs
+++ NLog0MQ.Example/Program.cs
@@ -1,23 +1,36 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
 
 namespace NLog0MQ.Example
 {
     public class Program
     {
+        private static readonly Logger Log = LogManager.GetLogger("NLog0MQ.Example");
         public static void Main(string[] args)
         {
-            DoMagic();
-
-            GC.Collect();
+            var cancellationTokenSource = new CancellationTokenSource();
+            Task.Factory.StartNew(()=>LogMessages(cancellationTokenSource.Token), TaskCreationOptions.LongRunning);
 
             Console.ReadLine();
+            cancellationTokenSource.Cancel();
         }
 
-        public static void DoMagic()
+        private static Action LogMessages(CancellationToken token)
         {
-            var log = NLog.LogManager.GetLogger("NLog0MQ.Example");
-            log.Info("Hello...");
-           
+            int j = 0;
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                j++;
+                var message = "Log message: " + j;
+                Log.Info(message);
+                Console.WriteLine(message);
+                Thread.Sleep(10);
+            }
+
+            return null;
         }
     }
 }
